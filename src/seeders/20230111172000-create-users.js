8d7d6c26261902107ef5bcd593c629669baf4c87
Tu Thanh Le <lethanhtu1551998@gmail.com>
2023-03-01 16:14:09 +0700

    fix: update seeder to create users

--- src/seeders/20230111172000-create-users.js
+++ src/seeders/20230111172000-create-users.js
@@ -1,47 +1,43 @@
 'use strict'
 const { v4: uuidv4 } = require('uuid')
+const bcrypt = require('bcrypt')
+
+const SALT_ROUNDS = 10
+
+const hashedPassword = async (password) => {
+  return await bcrypt.hash(password, SALT_ROUNDS)
+}
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-     */
     await queryInterface.bulkInsert('users', [
       {
         id: uuidv4(),
         email: 'admin@example.com',
-        password: 'Admin1234', // the password need to be hashed in future
+        password: await hashedPassword('Admin1234'),
         first_name: 'Tu',
         last_name: 'Le',
         full_name: 'admin',
         role: 2,
+        created_at: new Date(),
+        updated_at: new Date(),
       },
       {
         id: uuidv4(),
         email: 'user@example.com',
-        password: 'User1234',
+        password: await hashedPassword('User1234'),
         first_name: 'Tu',
         last_name: 'Le',
         full_name: 'user',
         role: 1,
+        created_at: new Date(),
+        updated_at: new Date(),
       },
     ])
   },
 
   async down(queryInterface) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
     await queryInterface.bulkDelete('users', null, {})
   },
 }
